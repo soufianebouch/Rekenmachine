2cc0e51771bdcc7ef9395decc87d835039e8900e
Student Education <Student@rdeducation.be>
2019-07-09 15:21:42 +0200

    Start project

---
+++ RekenamchineLogica/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RekenamchineLogica
+{
+    public class Class1
+    {
+        public static double Berekenen(double getal1, double getal2, string operat)
+        {
+            double result = 0;
+            switch (operat)
+            {
+                case "+":
+                    result = getal1 + getal2;
+                    break;
+                case "-":
+                    result = getal1 - getal2;
+                    break;
+                case "*":
+                    result = getal1 * getal2;
+                    break;
+                case "/":
+                    result = getal1 / getal2;
+                    break;
+            }
+            return result;
+        }
+    }
+}
